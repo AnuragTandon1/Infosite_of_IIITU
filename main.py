01fc196de97d8d29034504e7ff7b593360970b82
Anurag Tandon <135813224+AnuragTandon1@users.noreply.github.com>
2023-08-10 11:40:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,146 @@
+def acadamics():
+    print("_______________________________________________________________________________")
+    print("Under-Graduate Programmes\nB.Tech in Computer Science and Engineering (CSE) – 66 Seats\nB.Tech in Electronics and Communication Engineering (ECE) – 66 Seats\nB.Tech in Information Technology – 66 Seats")
+    print("_______________________________________________________________________________")
+    print("Enter 1 to know about acadamics fee.\nAny other key to go back to menu")
+
+    c=input()
+    if (c=='1'):
+        print("Institute fee structure for B.tech program\n______________________\n|1st sem | Rs1,25,000|\n|2nd sem | Rs1,20,000|\n|3rd sem | Rs1,20,000|\n|4th sem | Rs1,20,000|\n|5th sem | Rs1,20,000|\n|6th sem | Rs1,20,000|\n|7th sem | Rs1,20,000|\n|8th sem | Rs1,20,000|\n______________________\n ")
+def placement():
+    d=input("Enter \n1 for CSE\n2 for ECE\n3 for IT\n4 for overall")
+    if(d=='1'): 
+        print("Highest Package On Campus-Rs 25.5 LPA\nHighest Package Off Campus-Rs 47 LPA\nAverage Package -Rs 12.4 LPA\npercentage placed-94.12%\n")
+    elif(d=='2'):
+        print("Highest Package On Campus-Rs 25.5 LPA\nHighest Package Off Campus-Rs 44 LPA\nAverage Package -Rs 11.0 LPA\npercentage placed-85%\n")
+    elif(d=='3'):
+        print("Highest Package On Campus-Rs 16.6 LPA\nHighest Package Off Campus-Rs 44 LPA\nAverage Package -Rs 12.8 LPA\npercentage placed-93.75%\n")
+    elif(d=='4'):
+        print("Highest Package On Campus-Rs 25.5 LPA\nHighest Package Off Campus-Rs 47 LPA\nAverage Package -Rs 12.1 LPA\npercentage placed-90.77%\n")
+def  Recruitment():
+    d=input("Enter \n1 for school of Electronics\n2 for school of Computing\n")
+    if(d=='1'):
+        
+        
+        print("_______________________________________________________________")  
+        print("School of Electronics(SoE)\n_______________________________________________________________\nDate and time of written exam                :24 dec 2022 :10pm\nDate and time of interview of shortlisted    :24 dec 2022 :4pm\ncandidates(First 10 candidates) \nDate and time of interview of shortlisted    :25 dec 2022 :9.30pm\ncandidates(remaining candidates) ")
+        print("_______________________________________________________________")  
+     
+    elif(d=='2'):
+        print("_______________________________________________________________")    
+        print("School of Computing(SoC)\n_______________________________________________________________\nDate and time of written exam                :26 dec 2022 :10pm\nDate and time of interview of shortlisted    :26 dec 2022 :3pm\ncandidates(First 10 candidates) \nDate and time of interview of shortlisted    :27 dec 2022 :9.30pm\ncandidates(remaining candidates) ")
+        print("_______________________________________________________________")    
+        
+print("About IIIT UNA\n")
+print("\tIIIT Una is one of the 20 IIITs being setup, funded and managed by the Ministry of Human Resource Development, Govt. of India under the Public Private Partnership (PPP) model. The partners setting up IIIT Una are the Ministry of Human Resource Development, Govt. of India, the Govt. of Himachal Pradesh, HP Power Corporation Limited and HP Transmission Corporation Limited. Admissions to the undergraduate programmes in the Institute are made through the Joint Entrance Examination (JEE).\n\n\tAt present, IIIT Una operates from its permanent campus at Saloh, Una. The campus is fully furnished and working full fledged from its permanent campus.\n\n")
+print("\t\t\t****************Welcome to the portal*****************")
+def dapartments():
+    
+    d=input("Enter \n1 for school of computing\n2 for school of electronics\n3 for school of Basic sciences\n")
+    if(d=='1'):
+        print("______________________________________________________")
+        print("Department of computer science")
+        print("______________________________________________________")
+        print("|Prof. S. Selvakumar (Director) |director@iiitu.ac.in\n|Dr. Ambigavathi M              |ambigavathi@iiitu.ac.in\n|Mr. Divyansh Thakur            |divyansh@iiitu.ac.in\n|Ms. Hem Lata                   |hemlata@iiitu.ac.in\n|Ms. Himani Joshi               |himanijoshi@iiitu.ac.in\n|Dr. Jaspal Kaur Saini          |jaspalkaursaini@iiitu.ac\n|Ms. Ishita Vaidya              |ishita@iiitu.ac.in\n|Ms. Minakshi Shastri           |minakshishastri@iiitu.ac.in\n|Dr. Mrityunjay Singh           |mrityunjay singh@iiitu.ac.in\n|Dr. Neha Sharma                |neha@iiitu.ac.in\n|Dr Nishtha Hooda               |nishtha@iiitu.ac.in\n|Ms. Richa Thakur               |richa@iiitu.ac.in\n|Dr. Sahil                      |sahil.neelam@iiitu.ac.in\n|Dr. Sanjit Ningthoujam         |sanjit.n@iiitu.ac.in\n|Dr. Shatrughan Modi            |shatrughanmodi@iiitu.ac.in\n|Dr. ShivDutt Sharma            |shiv@iiitu.ac.in\n|Dr. Vikram Kumar               |vikram@iiitu.ac.in")
+        
+        print("______________________________________________________")
+    elif(d=='2'):
+        
+        print("______________________________________________________")
+        print("Department of Electronics ")
+        print("______________________________________________________")
+        print("|Dr. Ankur Kumar     |ankur@iiitu.ac.in\n|Dr. Gurpreet Kaur   |gurpreetkaur@iiitu.ac.in\n|Dr. Naman Garg      |namangarg@iiitu.ac.in\n|Dr. Naveen Cheggoju |naveen.c@iiitu.ac.in\n|Dr. Nitish Katal    |nitishkatal@iiitu.ac.in\n|Ms. Pranshu Sharma. |pranshu0214@iiitu.ac.in\n|Mr. Satish Kumar    |satish@iiitu.ac.in\n|Dr. Tanu            |tanu.wadhera@iiitu.ac.in\n|Dr. Vanita Arora    |vanita@iiitu.ac.in\n|Dr. Kishore Vejandla|kishorevejandla@iiitu.ac.in\n")
+    
+        print("______________________________________________________")
+    elif(d=='3'):
+        print("___________________________________________________________")
+        print("Department of Basic sciences ")
+        print("___________________________________________________________")
+        print("|Prof. A. N. Gill (Professor of Emeritus)|angill@iiitu.ac.in\n|Dr. Ankita Sharma                       |ankita@iiitu.ac.in\n|Dr. Gurinderbir S. Grewal               |gsgrewal@iiitu.ac.in\n|Dr. Madan L. Verma                      |madanverma@iiitu.ac.in\n|Ms. Manu Chauhan                        |manuchauhan@iiitu.ac.in\n|Dr. Payal Sharma                        |payal@iiitu.ac.in\n|Dr. Satendra Kumar                      |satendrak@iiitu.ac.in\n|Dr. Sukriti Chaudhary                   |sukriti@iiitu.ac.in\n|Dr. Vandita Sharma                      |vanditamaths@gmail.com")
+        
+        print("___________________________________________________________")
+def contact():
+    print("______________________________________________________")
+    print("Indian Institute of Information Technology Una\nPermanent Campus: Vill. Saloh, Teh. Haroli, Distt. Una \nHimachal Pradesh-177209\n______________________________________________________\nPhone no: 01975-257908\n______________________________________________________\nmail : director@iiitu.ac.in")
+    print("______________________________________________________")    
+def PG_admission():
+    d=input("Enter \n1 for CCMT (with stipend)\n2 for Institute counceling (without stipend)\n")
+    if(d=='1'):
+        print("______________________________________________________") 
+        
+        print("Total Seats : 15 Nos.\nApply at CCMT Portal.\nValid GATE Score in '''Computer Science and Information Technology''' is mandatory.")
+        print("______________________________________________________") 
+        print("Seat Matrix (With stipend)\nUR         :        07\nOBC        :        03\nSC         :        02\nST         :        01\nEWS        :        01\nUR-PwD     :        01")
+        print("______________________________________________________") 
+    if(d=='2'):
+        print("______________________________________________________") 
+        print("Total seats: 15 Nos.\nFill Online Registration Form\nRegistration Fee is Rs.1000/- only.")
+        print("______________________________________________________") 
+        print("Seat Matrix (Without stipend)\nUR         :        08\nOBC        :        03\nSC         :        02\nST         :        01\nEWS        :        01\nUR-PwD     :        01")
+        5
+    print("______________________________________________________") 
+
+def  UG_admission():
+    print("Admission will be completely on the basis of JEE Main rank and student have to apply in JOSSA and CSAB counseling")
+    
+    d=input("Enter \n1 for JOSSA round 1\n2 for JOSSA round 2\n3 for JOSSA round 3\n4 for JOSSA round 4\n5 for JOSSA round 5\n6 for JOSSA round 3\n7 for CSAB round 1\n8 for CSAB round 2\n")
+    
+    if(d=='1'):
+        print("\n________________________________\nCSE\n________________________________\n\nGeneral : 21897\nEWS     : 3750\nOBC     : 8004\nSC      : 5357\nST      : 2385\n________________________________\n")
+        print("\n________________________________\nECE\n________________________________\n\nGeneral : 30308\nEWS     : 4887\nOBC     : 11451\nSC      : 7006\nST      : 3524\n________________________________\n")
+        print("\n________________________________\nIT\n________________________________\n\nGeneral : 25535\nEWS     : 4111\nOBC     : 9063\nSC      : 6362\nST      : 3223\n________________________________\n")
+    if(d=='2'):
+        print("\n________________________________\nCSE\n________________________________\nGeneral : 23775\nEWS     : 3949\nOBC     : 8950\nSC      : 5552\nST      : 2540\n________________________________\n")
+        print("\n________________________________\nECE\n________________________________\n\nGeneral : 32839\nEWS     : 5237\nOBC     : 12657\nSC      : 7543\nST      : 4023\n________________________________\n")
+        print("\n________________________________\nIT\n________________________________\n\nGeneral : 27916\nEWS     : 4240\nOBC     : 10830\nSC      : 6813\nST      : 3346\n________________________________\n")
+    if(d=='3'):
+        print("\n________________________________\nCSE\n________________________________\nGeneral : 23800\nEWS     : 3949\nOBC     : 9058\nSC      : 5552\nST      : 2540\n________________________________\n")
+        print("\n________________________________\nECE\n________________________________\n\nGeneral : 33387\nEWS     : 5357\nOBC     : 12911\nSC      : 7547\nST      : 4037\n________________________________\n")
+        print("\n________________________________\nIT\n________________________________\n\nGeneral : 27973\nEWS     : 4290\nOBC     : 10862\nSC      : 6813\nST      : 3605\n________________________________\n")
+    if(d=='4'):
+        print("\n________________________________\nCSE\n________________________________\nGeneral : 24752\nEWS     : 3949\nOBC     : 9216\nSC      : 5552\nST      : 2540\n________________________________\n")
+        print("\n________________________________\nECE\n________________________________\n\nGeneral : 33519\nEWS     : 5357\nOBC     : 12937\nSC      : 7589\nST      : 4081\n________________________________\n")
+        print("\n________________________________\nIT\n________________________________\n\nGeneral : 28009\nEWS     : 4290\nOBC     : 10862\nSC      : 6813\nST      : 3605\n________________________________\n")
+    if(d=='5'):
+        print("\n________________________________\nCSE\n________________________________\nGeneral : 24858\nEWS     : 3949\nOBC     : 9216\nSC      : 5552\nST      : 2540\n________________________________\n")
+        print("\n________________________________\nECE\n________________________________\n\nGeneral : 33983\nEWS     : 5357\nOBC     : 12970\nSC      : 7681\nST      : 4081\n________________________________\n")
+        print("\n________________________________\nIT\n________________________________\n\nGeneral : 28808\nEWS     : 4364\nOBC     : 10862\nSC      : 6931\nST      : 3605\n________________________________\n")
+    if(d=='6'):
+        print("\n________________________________\nCSE\n________________________________\nGeneral : 26667\nEWS     : 4290\nOBC     : 9587\nSC      : 5989\nST      : 2540\n________________________________\n")
+        print("\n________________________________\nECE\n________________________________\n\nGeneral : 36676\nEWS     : 5667\nOBC     : 13287\nSC      : 7890\nST      : 4164\n________________________________\n")
+        print("\n________________________________\nIT\n________________________________\n\nGeneral : 30394\nEWS     : 4613\nOBC     : 11045\nSC      : 7036\nST      : 3901\n________________________________\n")
+    if(d=='7'):
+        print("\n________________________________\nCSE\n________________________________\nGeneral : 28150\nEWS     : 31247\nOBC     : 37492\nSC      : 116259\nST      : 221722 \n________________________________\n")
+        print("\n________________________________\nECE\n________________________________\n\nGeneral : 387019\nEWS     : 43911\nOBC     : 48615\nSC      : 158306\nST      : 264084\n________________________________\n")
+        print("\n________________________________\nIT\n________________________________\n\nGeneral : 31321\nEWS     : 37132\nOBC     : 41582\nSC      : 143511\nST      : 242724\n________________________________\n")
+    if(d=='8'):
+        print("\n________________________________\nCSE\n________________________________\nGeneral : 28234\nEWS     : 31247\nOBC     : 37492\nSC      : 117537\nST      : 221722\n________________________________\n")
+        print("\n________________________________\nECE\n________________________________\n\nGeneral : 40548\nEWS     : 49709\nOBC     : 51849\nSC      : 158992\nST      : 298757\n________________________________\n")
+        print("\n________________________________\nIT\n________________________________\n\nGeneral : 31492\nEWS     : 38534\nOBC     : 43501\nSC      : 145849\nST      : 255619\n________________________________\n")
+b='1'
+while(b=='1'):
+    print("Enter :")
+    print("\t1 to know about acadamics\n\t2 to know abot placement\n\t3 to know about departments\n\t4 to know about UG admission\n\t5 to know about PG admission\n\t6 to know about Recruitment\n\t7 to contact us\n\tpress any other key to exit...")
+    a=input()
+    if(a=='1'):
+        acadamics()
+    elif(a=='2'):
+        placement()
+    elif(a=='3'):
+        dapartments()
+    elif(a=='4'):
+        UG_admission()
+    elif(a=='5'):
+        PG_admission()
+    elif(a=='6'):
+        Recruitment()
+    elif(a=='7'):
+        contact()    
+    else:
+        break
+    
+    print("\n\nFor back to portal - please press 1\nany other key to exit")
+    b=input()
+    if (b!='1'):
+        break
+print("****************Thanks for visiting*************\n\tPlease visit again")
